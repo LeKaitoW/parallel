39c01dddc7a06e593de48c099b49b70951d140d3
Olga Zudina <le.chat.noir.kaito@gmail.com>
2017-03-11 22:54:56 +0300

    upd bisection print

--- bisection.py
+++ bisection.py
@@ -8,15 +8,11 @@ col = np.array([2, 6, 2, 4, 0, 1, 5, 4, 1, 3, 5, 2, 4, 0, 7, 10, 6, 8, 11,
 				7, 9, 12, 8, 13, 6, 11, 7, 10, 12, 8, 11, 13, 9, 12])
 A = coo_matrix((data, (row, col))).toarray()
 B = coo_matrix((data, (row, row))).toarray()
-print(A)
-print()
-print(B)
-print()
 C = np.subtract(B, A)
-print(C)
-print()
+print(A, B, C, sep='\n\n')
+
 eigvalues, eigvectors = np.linalg.eigh(C)
-print("eigvector =")
+print('\n' + 'eigvector =')
 print(eigvectors[1])
 average = np.average(eigvectors[1])
 print("average = ", average)
@@ -32,6 +28,6 @@ for index, eigvalue in enumerate(eigvector):
 	else:
 		second_graph.append(index)
 
-print(first_graph)
-print(second_graph)
+print('first graph:', first_graph)
+print('second graph:', second_graph)
 input()
